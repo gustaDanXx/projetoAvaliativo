91aaac98b346df761cd72babf18bb14b4357f622
CamilyL <camilycosme7@gmail.com>
2022-04-14 21:38:08 -0300

    app finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  Distancia: number = null
+  ConsumoGasolina: number = null
+  precoGasolina: number = null
+  Total: number = 0 
+  Litro: number = 0
   constructor() {}
+
+  CalcularGasto(){
+    this.Litro = this.Distancia / this.ConsumoGasolina;
+    this.Total = this.precoGasolina * this.Litro;
+  }
+
+
+
 
 }
